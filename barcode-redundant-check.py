24d0ca1141835f6871a91691544af4f6d12fddb6
Yessica Gomez <yessica.gomez@ucsf.edu>
2017-10-11 12:50:41 -0700

    more scripts

--- barcode-redundant-check.py
+++ barcode-redundant-check.py
@@ -17,7 +17,7 @@ pickle2 = sys.argv[2] # main input, barcode/mutation
 bcdict = pic.load(open(pickle2,"rb"))
 barcodes_off = bcdict.keys() # Pull barcodes from dict into list
 barcodes_act =[] # will be used below to pull barcodes from fastq file
-print len(fastq_file),len(bcdict),len(barcodes_off) # tells you size of inputs
+print len(barcodes_off) # tells you size of inputs
 
 ## pull out all the barcodes from the fastq
 # pattern = re.compile('[A-Z]{18}') # check for correct lines using re
@@ -29,16 +29,24 @@ print len(fastq_file),len(bcdict),len(barcodes_off) # tells you size of inputs
 #             barcodes_act.append(rawbc)
 # print len(barcodes_act), barcodes_act
 
+with open(fastq_file) as fp: # open the fastq file
+    for each in barcodes_off: # go through all barcodes for each line
+        print "MUTATION: %s\tBARCODE: %s" %(bcdict.get(each), each) # header for legibility
+        for line in enumerate(fp): # go through each line w/o storing all in memory
+            if each in line:
+                print each, line
+                location = line.split[-18:-1]
+                print location[0]
 # For each barcode, pull the corresponding location(s), and for given location(s), pull the corresponding aSyn sequence(s)
-for each in barcodes_off:
-    print "MUTATION: %s\tBARCODE: %s" %(bcdict.get(each), each) # header for legibility
-    locations = []
-    rawout = subprocess.check_output(["grep","%s %s" %(each,fastq_file)]) # grep the fastq file for the barcode seq
-    print rawout # DEBUG
-    loc = rawout.split(wt[-18:-1])
-    print loc # DEBUG
-    locations.append(loc) # all the locations that match the current barcode
-    print "%s possible sequences.\n" %(len(locations))
+# for each in barcodes_off:
+#     print "MUTATION: %s\tBARCODE: %s" %(bcdict.get(each), each) # header for legibility
+#     locations = []
+#     rawout = subprocess.check_output(["grep","%s %s" %(each,fastq_file)]) # grep the fastq file for the barcode seq
+#     print rawout # DEBUG
+#     loc = rawout.split(wt[-18:-1])
+#     print loc # DEBUG
+#     locations.append(loc) # all the locations that match the current barcode
+#     print "%s possible sequences.\n" %(len(locations))
     # Compare the seqs and spit back cases where diff > 0
     # for a, b in itertools.combinations(asynseq,2):
     #     print
--- makefakedicts.py
+++ makefakedicts.py
@@ -5,25 +5,32 @@ import itertools
 import re
 from collections import defaultdict
 
+# ' python SCRIPT FASTQ_FILE MUTDICT_PICKLE '
+
 name = sys.argv[1]
-keys = []
-values = []
+key = []
+value = []
 i = 0
-pattern = re.compile('[A-Z]')
+pattern = re.compile('[A-Z]{125}')
 
 with open(name,"r") as f:
     file = f.readlines()
     print "Read lines!"
     for line in file:
-        if "@M" == line[0:2]:
-            print line.rstrip()
-            keys.append(line.rstrip())
-        if len(line) == 27:
-#        if pattern.match(line):
-            values.append(line.rstrip())
+        # if "@M" == line[0:2]:
+        #     print line.rstrip()
+        #     keys.append(line.rstrip())
+#        if len(line) == 27:
+        if pattern.match(line):
+            line = line.rstrip()
+            key.append(line[-18:-1])
 
-print len(keys), len(values)
-dictionary = dict(itertools.izip(keys, values))
+pickle2 = sys.argv[2] # barcode/mutation
+bcdict = pic.load(open(pickle2,"rb"))
+value = bcdict.values() # Pull barcodes from dict into list
+
+print "Keys\tValues"
+print "%s\t%s\n" %(len(key), len(value))
+dictionary = dict(itertools.izip(key, value))
 print len(dictionary)
-
 pic.dump(dictionary, open( "dict2.pkl", "wb" ) )
